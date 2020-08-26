421829ef2d503cc61d2ed95ac74a578be4f6947a
Anton Panchenko <a.a.panchenkoo@gmail.com>
2020-08-27 01:42:16 +0300

    create filter animation and response

--- js/dist/catalog-products.js
+++ js/dist/catalog-products.js
@@ -1,20 +1,276 @@
 'use strict';
 
-var _filterModule = require('./modules/filter-module.js');
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var _filterModule2 = _interopRequireDefault(_filterModule);
+var Filters = function Filters(animationDuration) {
+    _classCallCheck(this, Filters);
 
-var _navigationModule = require('./modules/navigation-module.js');
+    _initialiseProps.call(this);
 
-var _navigationModule2 = _interopRequireDefault(_navigationModule);
+    this.animationDuration = animationDuration;
+    this.filters = document.querySelectorAll('.catalog-products-filter-item-header');
+    this.openFilterBtn = document.getElementById('filter_open');
+    this.filterCollapseBox = document.getElementById('filterSection');
+    this.isFilterCollapse = false;
+    this.isFilterAnimated = false;
+    this.overlay = document.querySelector('div.desktop-navigation-collapse-overlay');
+    this.createFilterEvents();
+    setTimeout(this.multiRange('range', 'rangeBetween', 'rangeButton1n', 'rangeButton2n', 'rangeInput1n', 'rangeInput2n'), 0);
+};
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _initialiseProps = function _initialiseProps() {
+    var _this = this;
 
-var navigation = new _navigationModule2.default(600);
-var filters = new _filterModule2.default(800);
+    this.createFilterEvents = function () {
+        // Разворачивание фильтров в меню фильтров
+        _this.filters.forEach(function (f) {
+            f.addEventListener('click', function (e) {
+                return _this.switchFilter(e, f);
+            });
+            f.addEventListener('touchend', function (e) {
+                return _this.switchFilter(e, f);
+            });
+        });
+        // Розворачивание и сворачивание меню фильтров
+        _this.openFilterBtn.addEventListener('click', _this.toggleFilterCollapse);
+        _this.openFilterBtn.addEventListener('touchend', _this.toggleFilterCollapse);
+        _this.overlay.addEventListener('click', _this.handleCloseFilter);
+        _this.overlay.addEventListener('touchend', _this.handleCloseFilter);
+    };
 
-document.getElementById('goToCartModalBtn').addEventListener('click', function (e) {
-    e.preventDefault();
-    $('#addCartModal').modal('hide');
-    navigation.openCart(e);
-});
+    this.switchFilter = function (e, f) {
+        e.preventDefault();
+        e.cancelBubble = true;
+        var filterNode = f.parentNode;
+        if (filterNode.classList.contains('filter-collapse')) {
+            filterNode.classList.remove('filter-collapse');
+        } else {
+            filterNode.classList.add('filter-collapse');
+        }
+    };
+
+    this.toggleFilterCollapse = function (e) {
+        e.preventDefault();
+        var isFilterCollapse = _this.isFilterCollapse;
+
+        isFilterCollapse ? _this.handleCloseFilter(e) : _this.handleOpenFilter(e);
+    };
+
+    this.handleOpenFilter = function (e) {
+        e.preventDefault();
+        var filterCollapseBox = _this.filterCollapseBox,
+            isFilterCollapse = _this.isFilterCollapse,
+            isFilterAnimated = _this.isFilterAnimated,
+            animationDuration = _this.animationDuration,
+            overlay = _this.overlay;
+
+        if (!isFilterCollapse && !isFilterAnimated && window.innerWidth < 860) {
+            _this.isFilterAnimated = true;
+            $(overlay).fadeIn(animationDuration * 0.75);
+            overlay.style.zIndex = '400';
+            $(filterCollapseBox).animate({ 'right': '0px' }, animationDuration * 0.75, function () {
+                _this.isFilterCollapse = true;
+                _this.isFilterAnimated = false;
+            });
+        }
+    };
+
+    this.handleCloseFilter = function (e) {
+        e.preventDefault();
+        var filterCollapseBox = _this.filterCollapseBox,
+            isFilterCollapse = _this.isFilterCollapse,
+            isFilterAnimated = _this.isFilterAnimated,
+            animationDuration = _this.animationDuration,
+            overlay = _this.overlay;
+
+        if (isFilterCollapse && !isFilterAnimated && window.innerWidth < 860) {
+            _this.isFilterAnimated = true;
+            $(overlay).fadeOut(animationDuration * 0.75);
+            overlay.style.zIndex = '10';
+            $(filterCollapseBox).animate({ 'right': '-400px' }, animationDuration * 0.75, function () {
+                _this.isFilterCollapse = false;
+                _this.isFilterAnimated = false;
+            });
+        }
+    };
+
+    this.multiRange = function (idX, btwX, btn1X, btn2X, input1, input2) {
+        var slider = document.getElementById(idX);
+        var between = document.getElementById(btwX);
+        var button1 = document.getElementById(btn1X);
+        var button2 = document.getElementById(btn2X);
+        var inpt1 = document.getElementById(input1);
+        var inpt2 = document.getElementById(input2);
+
+        var min = inpt1.min;
+        var max = inpt1.max;
+
+        /*init*/
+        var sliderCoords = getCoords(slider);
+        button1.style.marginLeft = '0px';
+        button2.style.marginLeft = 200 + 'px';
+        between.style.width = 200 + 'px';
+        inpt1.value = min;
+        inpt2.value = max;
+
+        inpt1.onchange = function (evt) {
+            if (parseInt(inpt1.value) < min) inpt1.value = min;
+            if (parseInt(inpt1.value) > max) inpt1.value = max;
+            if (parseInt(inpt1.value) > parseInt(inpt2.value)) {
+                var temp = inpt1.value;
+                inpt1.value = inpt2.value;
+                inpt2.value = temp;
+            }
+
+            var sliderCoords = getCoords(slider);
+            var per1 = parseInt(inpt1.value - min) * 100 / (max - min);
+            var per2 = parseInt(inpt2.value - min) * 100 / (max - min);
+            var left1 = per1 * (slider.offsetWidth - button1.offsetWidth) / 100;
+            var left2 = per2 * (slider.offsetWidth - button1.offsetWidth) / 100;
+
+            button1.style.marginLeft = left1 + 'px';
+            button2.style.marginLeft = left2 + 'px';
+
+            if (left1 > left2) {
+                between.style.width = left1 - left2 + 'px';
+                between.style.marginLeft = left2 + 'px';
+            } else {
+                between.style.width = left2 - left1 + 'px';
+                between.style.marginLeft = left1 + 'px';
+            }
+        };
+        inpt2.onchange = function (evt) {
+            if (parseInt(inpt2.value) < min) inpt2.value = min;
+            if (parseInt(inpt2.value) > max) inpt2.value = max;
+            if (parseInt(inpt1.value) > parseInt(inpt2.value)) {
+                var temp = inpt1.value;
+                inpt1.value = inpt2.value;
+                inpt2.value = temp;
+            }
+
+            var sliderCoords = getCoords(slider);
+            var per1 = parseInt(inpt1.value - min) * 100 / (max - min);
+            var per2 = parseInt(inpt2.value - min) * 100 / (max - min);
+            var left1 = per1 * (slider.offsetWidth - button1.offsetWidth) / 100;
+            var left2 = per2 * (slider.offsetWidth - button1.offsetWidth) / 100;
+
+            button1.style.marginLeft = left1 + 'px';
+            button2.style.marginLeft = left2 + 'px';
+
+            if (left1 > left2) {
+                between.style.width = left1 - left2 + 'px';
+                between.style.marginLeft = left2 + 'px';
+            } else {
+                between.style.width = left2 - left1 + 'px';
+                between.style.marginLeft = left1 + 'px';
+            }
+        };
+
+        /*mouse*/
+        button1.onmousedown = function (evt) {
+            var sliderCoords = getCoords(slider);
+            var betweenCoords = getCoords(between);
+            var buttonCoords1 = getCoords(button1);
+            var buttonCoords2 = getCoords(button2);
+            var shiftX2 = evt.pageX - buttonCoords2.left;
+            var shiftX1 = evt.pageX - buttonCoords1.left;
+
+            document.onmousemove = function (evt) {
+                var left1 = evt.pageX - shiftX1 - sliderCoords.left;
+                var right1 = slider.offsetWidth - button1.offsetWidth;
+                if (left1 < 0) left1 = 0;
+                if (left1 > right1) left1 = right1;
+                button1.style.marginLeft = left1 + 'px';
+
+                shiftX2 = evt.pageX - buttonCoords2.left;
+                var left2 = evt.pageX - shiftX2 - sliderCoords.left;
+                var right2 = slider.offsetWidth - button2.offsetWidth;
+                if (left2 < 0) left2 = 0;
+                if (left2 > right2) left2 = right2;
+
+                var per_min = 0;
+                var per_max = 0;
+                if (left1 > left2) {
+                    between.style.width = left1 - left2 + 'px';
+                    between.style.marginLeft = left2 + 'px';
+
+                    per_min = left2 * 100 / (slider.offsetWidth - button1.offsetWidth);
+                    per_max = left1 * 100 / (slider.offsetWidth - button1.offsetWidth);
+                } else {
+                    between.style.width = left2 - left1 + 'px';
+                    between.style.marginLeft = left1 + 'px';
+
+                    per_min = left1 * 100 / (slider.offsetWidth - button1.offsetWidth);
+                    per_max = left2 * 100 / (slider.offsetWidth - button1.offsetWidth);
+                }
+                inpt1.value = parseInt(min) + Math.round((max - min) * per_min / 100);
+                inpt2.value = parseInt(min) + Math.round((max - min) * per_max / 100);
+            };
+            document.onmouseup = function () {
+                document.onmousemove = document.onmouseup = null;
+            };
+            return false;
+        };
+
+        button2.onmousedown = function (evt) {
+            var sliderCoords = getCoords(slider);
+            var betweenCoords = getCoords(between);
+            var buttonCoords1 = getCoords(button1);
+            var buttonCoords2 = getCoords(button2);
+            var shiftX2 = evt.pageX - buttonCoords2.left;
+            var shiftX1 = evt.pageX - buttonCoords1.left;
+
+            document.onmousemove = function (evt) {
+                var left2 = evt.pageX - shiftX2 - sliderCoords.left;
+                var right2 = slider.offsetWidth - button2.offsetWidth;
+                if (left2 < 0) left2 = 0;
+                if (left2 > right2) left2 = right2;
+                button2.style.marginLeft = left2 + 'px';
+
+                shiftX1 = evt.pageX - buttonCoords1.left;
+                var left1 = evt.pageX - shiftX1 - sliderCoords.left;
+                var right1 = slider.offsetWidth - button1.offsetWidth;
+                if (left1 < 0) left1 = 0;
+                if (left1 > right1) left1 = right1;
+
+                var per_min = 0;
+                var per_max = 0;
+
+                if (left1 > left2) {
+                    between.style.width = left1 - left2 + 'px';
+                    between.style.marginLeft = left2 + 'px';
+                    per_min = left2 * 100 / (slider.offsetWidth - button1.offsetWidth);
+                    per_max = left1 * 100 / (slider.offsetWidth - button1.offsetWidth);
+                } else {
+                    between.style.width = left2 - left1 + 'px';
+                    between.style.marginLeft = left1 + 'px';
+                    per_min = left1 * 100 / (slider.offsetWidth - button1.offsetWidth);
+                    per_max = left2 * 100 / (slider.offsetWidth - button1.offsetWidth);
+                }
+                inpt1.value = parseInt(min) + Math.round((max - min) * per_min / 100);
+                inpt2.value = parseInt(min) + Math.round((max - min) * per_max / 100);
+            };
+            document.onmouseup = function () {
+                document.onmousemove = document.onmouseup = null;
+            };
+            return false;
+        };
+
+        button1.ondragstart = function () {
+            return false;
+        };
+        button2.ondragstart = function () {
+            return false;
+        };
+
+        function getCoords(elem) {
+            var box = elem.getBoundingClientRect();
+            return {
+                top: box.top + pageYOffset,
+                left: box.left + pageXOffset
+            };
+        }
+    };
+};
+
+var filters = new Filters(800);
--- js/src/catalog-products.js
+++ js/src/catalog-products.js
@@ -1,11 +1,277 @@
-import Filters from './modules/filter-module.js';
-import Navigation from './modules/navigation-module.js';
+class Filters {
+    constructor(animationDuration) {
+        this.animationDuration = animationDuration;
+        this.filters = document.querySelectorAll('.catalog-products-filter-item-header');
+        this.openFilterBtn = document.getElementById('filter_open');
+        this.filterCollapseBox = document.getElementById('filterSection');
+        this.isFilterCollapse = false;
+        this.isFilterAnimated = false;
+        this.overlay = document.querySelector('div.desktop-navigation-collapse-overlay');
+        this.createFilterEvents();
+        setTimeout(this.multiRange('range', 'rangeBetween', 'rangeButton1n', 'rangeButton2n', 'rangeInput1n', 'rangeInput2n'), 0);
+    }
 
-const navigation = new Navigation(600);
+    createFilterEvents = () => {
+        // Разворачивание фильтров в меню фильтров
+        this.filters.forEach((f) => {
+            f.addEventListener('click', (e) => this.switchFilter(e, f));
+            f.addEventListener('touchend', (e) => this.switchFilter(e, f));
+        })
+        // Розворачивание и сворачивание меню фильтров
+        this.openFilterBtn.addEventListener('click', this.toggleFilterCollapse);
+        this.openFilterBtn.addEventListener('touchend', this.toggleFilterCollapse);
+        this.overlay.addEventListener('click', this.handleCloseFilter);
+        this.overlay.addEventListener('touchend', this.handleCloseFilter);
+    }
+
+    switchFilter = (e, f) => {
+        e.preventDefault();
+        e.cancelBubble = true;
+        const filterNode = f.parentNode;
+        if(filterNode.classList.contains('filter-collapse')) {
+            filterNode.classList.remove('filter-collapse');
+        } else {
+            filterNode.classList.add('filter-collapse');
+        }
+    }
+
+    toggleFilterCollapse = (e) => {
+        e.preventDefault();
+        const { isFilterCollapse } = this;
+        isFilterCollapse ? this.handleCloseFilter(e) : this.handleOpenFilter(e);
+    }
+
+    handleOpenFilter = (e) => {
+        e.preventDefault();
+        const { filterCollapseBox, isFilterCollapse, isFilterAnimated, animationDuration, overlay } = this;
+        if(!isFilterCollapse && !isFilterAnimated && window.innerWidth < 860) {
+            this.isFilterAnimated = true;
+            $(overlay).fadeIn(animationDuration * 0.75);
+            overlay.style.zIndex = '400';
+            $(filterCollapseBox).animate({'right': '0px'}, animationDuration * 0.75, () => {
+                this.isFilterCollapse = true;
+                this.isFilterAnimated = false;
+            })
+        }
+    }
+
+    handleCloseFilter = (e) => {
+        e.preventDefault();
+        const { filterCollapseBox, isFilterCollapse, isFilterAnimated, animationDuration, overlay } = this;
+        if(isFilterCollapse && !isFilterAnimated && window.innerWidth < 860) {
+            this.isFilterAnimated = true;
+            $(overlay).fadeOut(animationDuration * 0.75);
+            overlay.style.zIndex = '10';
+            $(filterCollapseBox).animate({'right': '-400px'}, animationDuration * 0.75, () => {
+                this.isFilterCollapse = false;
+                this.isFilterAnimated = false;
+            })
+        }
+    }
+
+    multiRange = (idX, btwX, btn1X, btn2X, input1, input2) => {
+        let slider = document.getElementById(idX);
+        let between = document.getElementById(btwX);
+        let button1 = document.getElementById(btn1X);
+        let button2 = document.getElementById(btn2X);
+        let inpt1 = document.getElementById(input1);
+        let inpt2 = document.getElementById(input2);
+
+        let min = inpt1.min;
+        let max = inpt1.max;
+
+        /*init*/
+        let sliderCoords = getCoords(slider);
+        button1.style.marginLeft = '0px';
+        button2.style.marginLeft = 200 + 'px';
+        between.style.width = 200 + 'px';
+        inpt1.value = min;
+        inpt2.value = max;
+
+        inpt1.onchange= function(evt)
+        {
+            if (parseInt(inpt1.value) < min)
+                inpt1.value = min;
+            if (parseInt(inpt1.value) > max)
+                inpt1.value = max;
+            if (parseInt(inpt1.value) > parseInt(inpt2.value))
+            {
+                let temp = inpt1.value;
+                inpt1.value = inpt2.value;
+                inpt2.value = temp;
+            }
+
+
+            let sliderCoords = getCoords(slider);
+            let per1 = parseInt(inpt1.value-min)*100/(max-min);
+            let per2 = parseInt(inpt2.value-min)*100/(max-min);
+            let left1 = per1*(slider.offsetWidth-button1.offsetWidth)/100;
+            let left2 = per2*(slider.offsetWidth-button1.offsetWidth)/100;
+
+            button1.style.marginLeft = left1 + 'px';
+            button2.style.marginLeft = left2 + 'px';
+
+            if (left1 > left2)
+            {
+                between.style.width = (left1-left2) + 'px';
+                between.style.marginLeft = left2 + 'px';
+            }
+            else
+            {
+                between.style.width = (left2-left1) + 'px';
+                between.style.marginLeft = left1 + 'px';
+            }
+        }
+        inpt2.onchange= function(evt)
+        {
+            if (parseInt(inpt2.value) < min)
+                inpt2.value = min;
+            if (parseInt(inpt2.value) > max)
+                inpt2.value = max;
+            if (parseInt(inpt1.value) > parseInt(inpt2.value))
+            {
+                var temp = inpt1.value;
+                inpt1.value = inpt2.value;
+                inpt2.value = temp;
+            }
+
+            let sliderCoords = getCoords(slider);
+            let per1 = parseInt(inpt1.value-min)*100/(max-min);
+            let per2 = parseInt(inpt2.value-min)*100/(max-min);
+            let left1 = per1*(slider.offsetWidth-button1.offsetWidth)/100;
+            let left2 = per2*(slider.offsetWidth-button1.offsetWidth)/100;
+
+            button1.style.marginLeft = left1 + 'px';
+            button2.style.marginLeft = left2 + 'px';
+
+            if (left1 > left2)
+            {
+                between.style.width = (left1-left2) + 'px';
+                between.style.marginLeft = left2 + 'px';
+            }
+            else
+            {
+                between.style.width = (left2-left1) + 'px';
+                between.style.marginLeft = left1 + 'px';
+            }
+        }
+
+        /*mouse*/
+        button1.onmousedown = function(evt) {
+            let sliderCoords = getCoords(slider);
+            let betweenCoords = getCoords(between);
+            let buttonCoords1 = getCoords(button1);
+            let buttonCoords2 = getCoords(button2);
+            let shiftX2 = evt.pageX - buttonCoords2.left;
+            let shiftX1 = evt.pageX - buttonCoords1.left;
+
+            document.onmousemove = function(evt) {
+                let left1 = evt.pageX - shiftX1 - sliderCoords.left;
+                let right1 = slider.offsetWidth - button1.offsetWidth;
+                if (left1 < 0) left1 = 0;
+                if (left1 > right1) left1 = right1;
+                button1.style.marginLeft = left1 + 'px';
+
+
+                shiftX2 = evt.pageX - buttonCoords2.left;
+                let left2 = evt.pageX - shiftX2 - sliderCoords.left;
+                let right2 = slider.offsetWidth - button2.offsetWidth;
+                if (left2 < 0) left2 = 0;
+                if (left2 > right2) left2 = right2;
+
+                let per_min = 0;
+                let per_max = 0;
+                if (left1 > left2)
+                {
+                    between.style.width = (left1-left2) + 'px';
+                    between.style.marginLeft = left2 + 'px';
+
+                    per_min = left2*100/(slider.offsetWidth-button1.offsetWidth);
+                    per_max = left1*100/(slider.offsetWidth-button1.offsetWidth);
+                }
+                else
+                {
+                    between.style.width = (left2-left1) + 'px';
+                    between.style.marginLeft = left1 + 'px';
+
+                    per_min = left1*100/(slider.offsetWidth-button1.offsetWidth);
+                    per_max = left2*100/(slider.offsetWidth-button1.offsetWidth);
+                }
+                inpt1.value= (parseInt(min)+Math.round((max-min)*per_min/100));
+                inpt2.value= (parseInt(min)+Math.round((max-min)*per_max/100));
+
+            };
+            document.onmouseup = function() {
+                document.onmousemove = document.onmouseup = null;
+            };
+            return false;
+        };
+
+        button2.onmousedown = function(evt) {
+            let sliderCoords = getCoords(slider);
+            let betweenCoords = getCoords(between);
+            let buttonCoords1 = getCoords(button1);
+            let buttonCoords2 = getCoords(button2);
+            let shiftX2 = evt.pageX - buttonCoords2.left;
+            let shiftX1 = evt.pageX - buttonCoords1.left;
+
+            document.onmousemove = function(evt) {
+                let left2 = evt.pageX - shiftX2 - sliderCoords.left;
+                let right2 = slider.offsetWidth - button2.offsetWidth;
+                if (left2 < 0) left2 = 0;
+                if (left2 > right2) left2 = right2;
+                button2.style.marginLeft = left2 + 'px';
+
+
+                shiftX1 = evt.pageX - buttonCoords1.left;
+                let left1 = evt.pageX - shiftX1 - sliderCoords.left;
+                let right1 = slider.offsetWidth - button1.offsetWidth;
+                if (left1 < 0) left1 = 0;
+                if (left1 > right1) left1 = right1;
+
+                let per_min = 0;
+                let per_max = 0;
+
+                if (left1 > left2)
+                {
+                    between.style.width = (left1-left2) + 'px';
+                    between.style.marginLeft = left2 + 'px';
+                    per_min = left2*100/(slider.offsetWidth-button1.offsetWidth);
+                    per_max = left1*100/(slider.offsetWidth-button1.offsetWidth);
+                }
+                else
+                {
+                    between.style.width = (left2-left1) + 'px';
+                    between.style.marginLeft = left1 + 'px';
+                    per_min = left1*100/(slider.offsetWidth-button1.offsetWidth);
+                    per_max = left2*100/(slider.offsetWidth-button1.offsetWidth);
+                }
+                inpt1.value= (parseInt(min)+Math.round((max-min)*per_min/100));
+                inpt2.value= (parseInt(min)+Math.round((max-min)*per_max/100));
+
+            };
+            document.onmouseup = function() {
+                document.onmousemove = document.onmouseup = null;
+            };
+            return false;
+        };
+
+        button1.ondragstart = function() {
+            return false;
+        };
+        button2.ondragstart = function() {
+            return false;
+        };
+
+        function getCoords(elem) {
+            let box = elem.getBoundingClientRect();
+            return {
+                top: box.top + pageYOffset,
+                left: box.left + pageXOffset
+            };
+        }
+    }
+
+}
+
 const filters = new Filters(800);
-
-document.getElementById('goToCartModalBtn').addEventListener('click', (e) => {
-    e.preventDefault();
-    $('#addCartModal').modal('hide');
-    navigation.openCart(e);
-})
