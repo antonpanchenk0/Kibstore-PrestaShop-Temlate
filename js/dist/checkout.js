efe3b692e7ff527b998445b51c0d8f0f90bd91be
Anton Panchenko <a.a.panchenkoo@gmail.com>
2020-09-08 00:02:22 +0300

    checkout page

--- js/dist/checkout.js
+++ js/dist/checkout.js
@@ -1,19 +1,19 @@
 'use strict';
 
 var inputs = {
-    user_name: {
+    userName: {
         node: document.getElementById('uname'),
         regExp: /[a-z\u017F\u0430-\u044F\u0454\u0456\u0457\u0491\u212A]/gmi
     },
-    user_phone: {
+    userPhone: {
         node: document.getElementById('uphone'),
         regExp: /^([+38|38]){0,3}([0-9]){10}$/mg
     },
-    user_email: {
+    userEmail: {
         node: document.getElementById('uemail'),
         regExp: /^[a-z0-9._]+@[a-z]+\.+[a-z]{1,8}$/mgi
     },
-    delivery_type: {
+    deliveryType: {
         nodes: document.querySelectorAll('input[name=delivery_type]'),
         delivery_mail_number: document.getElementById('mail_num'),
         delivery_mail_number_regExp: /^[0-9]{1,}$/g,
@@ -21,12 +21,14 @@ var inputs = {
         delivery_address_regExp: /[a-zа-яґїієй\s.\/0-9]/gmi,
         isSelected: false
     },
-    delivery_city: {
+    deliveryCity: {
         node: document.getElementById('dcity'),
         regExp: /^[A-Za-zА-Яа-яЁё]{1,}$/
     },
-    payment_type: {
+    paymentType: {
         nodes: document.querySelectorAll('input[name=payment]')
     }
 
 };
+
+$(user_phone).mask("+37(999)999-9999");
